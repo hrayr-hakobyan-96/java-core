9d8054faf184a62771e9fff9ac0f37e1fc9b51f1
USER <hakobyan.hrayr.26@gmail.com>
2023-08-29 23:12:54 +0400

    homework2

--- src/homework/Masiv.java
+++
@@ -1,7 +0,0 @@
-package homework;
-
-public class Masiv {
-    public static void main(String[] args) {
-        System.out.println(1);
-     }
-}
--- src/homework/hom/Homework1.java
+++ src/homework/hom/Homework1.java
@@ -1,4 +1,4 @@
-package homework;
+package homework.hom;
 
 public class Homework1 {
     public static void main(String[] args) {
