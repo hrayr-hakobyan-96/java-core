f836477091d8ca86f38f480a48cd45dc8fcf3434
USER <hakobyan.hrayr.26@gmail.com>
2023-09-07 00:12:50 +0400

    ForExamples

---
+++ src/homwork/ArrayUtil.java
@@ -0,0 +1,121 @@
+package homwork;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 6, 3, 9, 15, 52, -3, 5, 8,};
+        // Տպել բոլոր էլեմենտները իրար կողք, բաժանված պռաբելով։
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+        //Տպել մասիվի առաջին էլեմենտը
+        System.out.println("the first element of the array=" + numbers[0]);
+        //Տպել մասիվի վերջին էլեմենտը
+        int LastElement = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            LastElement = i;
+        }
+        System.out.println("the last element of the array=" + LastElement);
+        //Տպել մասիվի երկարությունը
+        System.out.println("the length of the array=" + numbers.length);
+        //Տպել մասիվի ամենափոքր թիվը
+        int min = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            if (min > numbers[i])
+                min = numbers[i];
+        }
+        System.out.println("min=" + min);
+
+        int array = 0;
+        int elements = 0;
+        //Տպել մասիվի մեջտեղի թիվը, ստուգել որ 2-ից մեծ լինի երկարությունը, եթե փոքր կամ հավասար էղավ, տպեք որ
+        // can't print middle values. եթե զույգ է մասիվի մեջի էլեմենտների թիվը, տպեք մեջտեղի երկու էլեմենտները,
+        // եթե կենտ է մի հատը։
+        int cnetr = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            cnetr = numbers[numbers.length / 2];
+        }
+        if (cnetr<=2)
+            System.out.println("can't print middle values");
+        if (numbers.length % 2 == 0)
+            System.out.println(numbers[(numbers.length / 2) - 1] + " " + numbers[numbers.length / 2]);
+        else {
+            System.out.println(numbers[numbers.length / 2]);
+
+        }
+        System.out.println("centr array=" + cnetr);
+
+
+    }
+
+    public static class Homework1 {
+        public static void main(String[] args) {
+            //1//
+    //        for (int i = 0; i <= 5; i++) {
+    //            for (int j = 0; j < i; j++) {
+    //                System.out.print("* ");
+    //            }
+    //            System.out.println(" ");
+    //
+    //
+    //        }
+    //        //2//
+    //        System.out.println();
+    //        for (int i = 0; i < 5; i++) {
+    //            for (int j = 5; j > i; j--) {
+    //                System.out.print(" *");
+    //            }
+    //            System.out.println();
+    //        }
+    //        //3//
+    //        System.out.println();
+    //        for (int i = 0; i < 5; i++) {
+    //            for (int j = 5; j > i; j--) {
+    //                System.out.print("  ");
+    //            }
+    //            for (int j = 0; j <= i; j++) {
+    //                System.out.print("* ");
+    //            }
+    //            System.out.println();
+    //        }
+    //        //4//
+    //        System.out.println();
+    //        for (int i = 0; i < 5; i++) {
+    //            for (int j = 0; j < i; j++) {
+    //                System.out.print("  ");
+    //            }
+    //
+    //            for (int j = i; j < 5; j++) {
+    //                System.out.print(" *");
+    //            }
+    //            System.out.println();
+    //        }
+    //        //5//
+    //        System.out.println();
+    //        for (int i = 0; i < 4; i++) {
+    //            for (int j = 4; j > i; j--) {
+    //                System.out.print("  ");
+    //            }
+    //            for (int j = 0; j <= i; j++) {
+    //                System.out.print(" * ");
+    //            }
+    //            System.out.println();
+    //        }
+    //        for (int i = 0; i < 5; i++) {
+    //            for (int j = 0; j <= i; j++) {
+    //                System.out.print(" ");
+    //            }
+    //            for (int j = i; j < 5; j++) {
+    //                System.out.print(" * ");
+    //            }
+    //            System.out.println();
+    //        }
+
+
+        }
+
+    }
+}
+
+
---
+++ src/homwork/ForExamples.java
@@ -0,0 +1,71 @@
+package homwork;
+
+public class ForExamples {
+    public static void main(String[] args) {
+
+        int[] number= new int[1001];
+        for (int i = 1; i <= number.length; i++) {
+            number[i] = i + 1;
+            System.out.print(i);
+            char[] sim = new char[1];
+            for (int j = i; j < number.length - 1; j++) {
+                sim[0] = '-';
+            }
+            System.out.print(sim);
+        }
+        int[] array = new int[101];
+        for (int k = 1; k <= array.length-1; k++) {
+            array[k] = k + 1;
+            if (array[k]%2!=0){
+                System.out.print(k+" ");
+            }
+        }
+        System.out.println();
+
+        int[] num = {2, 5, 8, 4, 9, 3, 7};
+       int max=num[0];
+        for (int i = 0; i < num.length; i++) {
+            if (num[i]>max){
+                max=num[i];
+            }
+        }
+        System.out.println();
+        System.out.println(max);
+        int []numbrs={4,8,6,9,25};
+        int[]nam=new int[ numbrs.length];
+        for (int i = 0; i < number.length; i++) {
+            if (numbrs[i]<numbrs[i]){
+
+            }
+            System.out.print(nam);
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
